817ee40d3f8992003787ffe8060e39adbc25f4c2
kv778 <54717308+kv778@users.noreply.github.com>
2020-02-14 09:48:07 +0530

    Add files via upload
    Fixed aesthetics

--- nfa_to_dfa.py
+++ nfa_to_dfa.py
@@ -2,18 +2,16 @@ nfa = {}
 
 n = int(input("Enter the no. of states: "))
 
-
 for i in range(n):
+    print("\n")
     state = input("Enter state name : ")
     no_t = int(input("Enter no. of transitions from state {}: ".format(state)))
     trans_of_state = {}
     for j in range(no_t):
         transition = input("Enter transition {}: ".format(j+1))
-        end_state = input("Enter end state of the transition: ")
+        end_state = input("Enter end states of the transition: ")
         trans_of_state[transition] = end_state   
     nfa[state] = trans_of_state
-       
-        
         
 print("\nEntered NFA : \n")
 print(nfa)            
