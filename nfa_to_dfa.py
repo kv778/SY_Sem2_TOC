cf14d885737c3e6b52d8c8f5420e9c513a017ef3
kv778 <54717308+kv778@users.noreply.github.com>
2020-02-14 09:33:26 +0530

    Add files via upload

---
+++ nfa_to_dfa.py
@@ -0,0 +1,19 @@
+nfa = {}
+
+n = int(input("Enter the no. of states: "))
+
+
+for i in range(n):
+    state = input("Enter state name : ")
+    no_t = int(input("Enter no. of transitions from state {}: ".format(state)))
+    trans_of_state = {}
+    for j in range(no_t):
+        transition = input("Enter transition {}: ".format(j+1))
+        end_state = input("Enter end state of the transition: ")
+        trans_of_state[transition] = end_state   
+    nfa[state] = trans_of_state
+       
+        
+        
+print("\nEntered NFA : \n")
+print(nfa)            
