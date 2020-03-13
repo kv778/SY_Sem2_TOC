204761cb7f77df368ffd693f9678c5c41c1b413f
kv778 <54717308+kv778@users.noreply.github.com>
2020-03-13 09:19:21 +0530

    Create nfa_to_dfa_space_edition.py

---
+++ nfa_to_dfa_space_edition.py
@@ -0,0 +1,19 @@
+nfa = {}
+    
+n = int(input("Enter the no. of states: "))
+    
+for i in range(n):
+    print("\n")
+    state = input("Enter state name : ")
+    no_t = int(input("Enter no. of transitions from state {}: ".format(state)))
+    trans_of_state = {}
+    for j in range(no_t):
+        transition_input=[]
+        trans_input = input("Enter input for transition {}: ".format(j+1))
+        transition_input.append(trans_input)
+        end_state = input("Enter end state of the transition: ")
+        trans_of_state[transition_input] = end_state   
+    nfa[state] = trans_of_state
+            
+print("\nEntered NFA : \n")
+print(nfa)
