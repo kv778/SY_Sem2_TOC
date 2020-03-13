1806340edb3372cc643f73493ea0cb9dfb4432d6
kv778 <54717308+kv778@users.noreply.github.com>
2020-03-13 08:32:35 +0530

    Update nfa_to_dfa.py

--- nfa_to_dfa.py
+++ nfa_to_dfa.py
@@ -8,9 +8,9 @@ for i in range(n):
     no_t = int(input("Enter no. of transitions from state {}: ".format(state)))
     trans_of_state = {}
     for j in range(no_t):
-        transition = input("Enter transition {}: ".format(j+1))
+        transition_inut = input("Enter transition {}: ".format(j+1))
         end_state = input("Enter end states of the transition: ")
-        trans_of_state[transition] = end_state   
+        trans_of_state[transition_input] = end_state   
     nfa[state] = trans_of_state
         
 print("\nEntered NFA : \n")
