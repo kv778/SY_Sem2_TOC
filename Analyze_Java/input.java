6f0dc433010117ea9462f46c7ade8807cdfc3381
kv778 <54717308+kv778@users.noreply.github.com>
2020-04-29 08:29:23 -0400

    Add files via upload

---
+++ Analyze_Java/input.java
@@ -0,0 +1,11 @@
+class input
+{
+	public static void main(String args[])
+	{
+		System.out.println("This is the input file.");
+		int a=3,b=2;
+		int c;
+		c = a + b;
+		System.out.println("Sum: ",c);
+	}
+}
